42bf14afc4e92346662266722b94e3fc357e2cd6
kupuma-ru21 <kupumaru21@gmail.com>
2021-06-22 13:12:48 +0900

    refactor: move input to a separate file

---
+++ src/presentation/components/input/input.tsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import Styles from './input-styles.scss';
+
+type Props = React.DetailedHTMLProps<
+  React.InputHTMLAttributes<HTMLInputElement>,
+  HTMLInputElement
+>;
+
+const Input: React.VFC<Props> = (props: Props) => {
+  return (
+    <div className={Styles.inputWrap}>
+      <input {...props} />
+      <span className={Styles.status}>🔴</span>
+    </div>
+  );
+};
+
+export default Input;
