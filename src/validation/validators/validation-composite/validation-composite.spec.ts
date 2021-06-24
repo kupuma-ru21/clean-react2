f815cf9c96f4fa7003c3d83abe6b2bd5f76b95bd
kupuma-ru21 <kupumaru21@gmail.com>
2021-06-24 23:47:54 +0900

    chore: typo

--- src/validation/validators/validation-composite/validation-composite.spec.ts
+++ src/validation/validators/validation-composite/validation-composite.spec.ts
@@ -4,25 +4,25 @@ import { ValidationComposite } from './validation-composite';
 
 type SutTyeps = {
   sut: ValidationComposite;
-  fieldalidationsSpy: FieldValidationSpy[];
+  fieldValidationsSpy: FieldValidationSpy[];
 };
 
 const makeSut = (fieldName: string): SutTyeps => {
   const fieldValidationSpy = new FieldValidationSpy(fieldName);
   const fieldValidationSpy2 = new FieldValidationSpy(fieldName);
-  const fieldalidationsSpy = [fieldValidationSpy, fieldValidationSpy2];
-  const sut = ValidationComposite.build(fieldalidationsSpy);
+  const fieldValidationsSpy = [fieldValidationSpy, fieldValidationSpy2];
+  const sut = ValidationComposite.build(fieldValidationsSpy);
 
-  return { sut, fieldalidationsSpy };
+  return { sut, fieldValidationsSpy };
 };
 
 describe('ValidationComposite', () => {
   test('Should return error if any validation fails', () => {
     const fieldName = faker.database.column();
-    const { sut, fieldalidationsSpy } = makeSut(fieldName);
+    const { sut, fieldValidationsSpy } = makeSut(fieldName);
     const errorFirstMessage = faker.random.words();
-    fieldalidationsSpy[0].error = new Error(errorFirstMessage);
-    fieldalidationsSpy[1].error = new Error(faker.random.words());
+    fieldValidationsSpy[0].error = new Error(errorFirstMessage);
+    fieldValidationsSpy[1].error = new Error(faker.random.words());
     const error = sut.validate(fieldName, faker.random.word());
     expect(error).toBe(errorFirstMessage);
   });
