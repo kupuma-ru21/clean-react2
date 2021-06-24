e0cf6c93dd23f3c80b2fa7a82d0b477cbb665042
kupuma-ru21 <kupumaru21@gmail.com>
2021-06-24 19:55:00 +0900

    feat: ensure EmailValidation returns falsy if email is valid

--- src/validation/validators/email/email-validation.ts
+++ src/validation/validators/email/email-validation.ts
@@ -4,6 +4,8 @@ import { FieldValidation } from '@/validation/procotols/field-validation';
 export class EmailValidation implements FieldValidation {
   constructor(readonly field: string) {}
   validate(value: string): Error {
-    return new InvalidFieldError();
+    const emailRegex =
+      /^[A-Za-z0-9]{1}[A-Za-z0-9_.-]*@{1}[A-Za-z0-9_.-]{1,}\.[A-Za-z0-9]{1,}$/;
+    return emailRegex.test(value) ? null : new InvalidFieldError();
   }
 }
