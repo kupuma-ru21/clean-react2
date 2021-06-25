64d48f15b512db487032984ef9686f6ea7cfb4ee
kupuma-ru21 <kupumaru21@gmail.com>
2021-06-25 23:41:55 +0900

    refactor: updata eslint-standard-with-typescript version

--- src/domain/usecases/authentication.ts
+++ src/domain/usecases/authentication.ts
@@ -3,5 +3,5 @@ import { AccountModel } from '@/domain/models/account-model';
 export type AuthenticationParams = { email: string; password: string };
 
 export interface Authentication {
-  auth(params: AuthenticationParams): Promise<AccountModel>;
+  auth: (params: AuthenticationParams) => Promise<AccountModel>;
 }
