63b7e3bda8efb41b8ef9474bbad6bb7e47a15e16
kupuma-ru21 <kupumaru21@gmail.com>
2021-06-26 18:49:21 +0900

    refactor: rename data mocks

--- src/data/test/mock-http-post.ts
+++
@@ -1,7 +0,0 @@
-import faker from 'faker';
-import { HttpPostParams } from '@/data/procotols/http';
-
-export const mockPostRequest = (): HttpPostParams<any> => ({
-  url: faker.internet.url(),
-  body: faker.random.objectElement(),
-});
