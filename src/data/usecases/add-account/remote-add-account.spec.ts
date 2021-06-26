dd4fe1fe91d2e077d44eba5aa6aaa8893ac22497
kupuma-ru21 <kupumaru21@gmail.com>
2021-06-26 20:28:34 +0900

    test: ensure RemoteAccountModel return an AccountModel on suceess

--- src/data/usecases/add-account/remote-add-account.spec.ts
+++ src/data/usecases/add-account/remote-add-account.spec.ts
@@ -1,5 +1,5 @@
 import faker from 'faker';
-import { mockAddAccountParams } from '@/domain/test';
+import { mockAccountModel, mockAddAccountParams } from '@/domain/test';
 import { AddAccountParams } from '@/domain/usecases';
 import { AccountModel } from '@/domain/models/';
 import { EmainInUseError, UnexpectedError } from '@/domain/errors';
@@ -62,5 +62,16 @@ describe('RemoteAddAccount', () => {
     httpPostClientSpy.response = { statusCode: HttpStatusCode.notFount };
     const promise = sut.add(mockAddAccountParams());
     await expect(promise).rejects.toThrow(new UnexpectedError());
+  });
+
+  test('Should return an AccountModel if HttpPostClient returns 200', async () => {
+    const { sut, httpPostClientSpy } = makeSut();
+    const httpResult = mockAccountModel();
+    httpPostClientSpy.response = {
+      statusCode: HttpStatusCode.ok,
+      body: httpResult,
+    };
+    const account = await sut.add(mockAddAccountParams());
+    expect(account).toEqual(httpResult);
   });
 });
