0a9a444b0503b667a96ea31e43d99e62daad9813
kupuma-ru21 <kupumaru21@gmail.com>
2021-06-26 23:43:26 +0900

    test: ensure SignUp change name status to error if Validation fails

--- src/presentation/pages/signup/signup.spec.tsx
+++ src/presentation/pages/signup/signup.spec.tsx
@@ -1,12 +1,18 @@
 import React from 'react';
-import { RenderResult, render } from '@testing-library/react';
+import {
+  RenderResult,
+  render,
+  cleanup,
+  fireEvent,
+} from '@testing-library/react';
+import faker from 'faker';
 import { ValidationStub, Helper } from '@/presentation/test';
 import SignUp from './signup';
 
 type SutTypes = { sut: RenderResult; validationStub: ValidationStub };
 const makeSut = (): SutTypes => {
   const validationStub = new ValidationStub();
-  const sut = render(<SignUp />);
+  const sut = render(<SignUp validation={validationStub} />);
   return { sut, validationStub };
 };
 
@@ -19,15 +25,33 @@ const testChildCount = (
   expect(el.childElementCount).toBe(count);
 };
 
+const populateField = (
+  sut: RenderResult,
+  fieldName: string,
+  value = faker.random.word()
+): void => {
+  const input = sut.getByTestId(fieldName);
+  fireEvent.input(input, { target: { value } });
+};
+
 describe('SignUp Component', () => {
+  afterEach(cleanup);
+
   test('Should start with initial state', () => {
     const { sut, validationStub } = makeSut();
     testChildCount(sut, 'error-wrap', 0);
     Helper.testButtonDisabled(sut, 'submit', true);
-    validationStub.errorMessage = 'error';
     Helper.testStatusForField(sut, 'name', validationStub);
+    validationStub.errorMessage = 'error';
     Helper.testStatusForField(sut, 'email', validationStub);
     Helper.testStatusForField(sut, 'password', validationStub);
     Helper.testStatusForField(sut, 'passwordConfirmation', validationStub);
+  });
+
+  test('Should show name error if Validation fails', () => {
+    const { sut, validationStub } = makeSut();
+    validationStub.errorMessage = faker.random.words();
+    populateField(sut, 'name');
+    Helper.testStatusForField(sut, 'name', validationStub);
   });
 });
