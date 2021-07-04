1e525d3582ed08dc0039d0f000fa3b6c495feee4
kupuma-ru21 <kupumaru21@gmail.com>
2021-07-04 15:43:45 +0900

    test: ensure Login present error if form is invalid

--- src/main/test/cypress/integration/login.spec.ts
+++ src/main/test/cypress/integration/login.spec.ts
@@ -1,12 +1,33 @@
+import faker from 'faker';
+
 describe('Login', () => {
   beforeEach(() => cy.visit('login'));
   it('Should load with correct initial state', () => {
+    cy.getByTestId('email').should('have.attr', 'readOnly');
     cy.getByTestId('email-status')
       .should('have.attr', 'title', '入力必須です')
       .should('contain.text', '🔴');
+
+    cy.getByTestId('password').should('have.attr', 'readOnly');
     cy.getByTestId('password-status')
       .should('have.attr', 'title', '入力必須です')
       .should('contain.text', '🔴');
+
+    cy.getByTestId('submit').should('have.attr', 'disabled');
+    cy.getByTestId('error-wrap').should('not.have.descendants');
+  });
+
+  it('Should present error if form is invalid', () => {
+    cy.getByTestId('email').focus().type(faker.random.word());
+    cy.getByTestId('email-status')
+      .should('have.attr', 'title', '無効な値')
+      .should('contain.text', '🔴');
+
+    cy.getByTestId('password').focus().type(faker.random.alphaNumeric(3));
+    cy.getByTestId('password-status')
+      .should('have.attr', 'title', '無効な値')
+      .should('contain.text', '🔴');
+
     cy.getByTestId('submit').should('have.attr', 'disabled');
     cy.getByTestId('error-wrap').should('not.have.descendants');
   });
