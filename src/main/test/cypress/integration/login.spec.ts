6c0e4af8092e26278a5cc3f1011bf4e903198327
kupuma-ru21 <kupumaru21@gmail.com>
2021-07-04 14:13:28 +0900

    chore: add cypress command

--- src/main/test/cypress/integration/login.spec.ts
+++ src/main/test/cypress/integration/login.spec.ts
@@ -1,5 +1,6 @@
 describe('Login', () => {
+  beforeEach(() => cy.visit('login'));
   it('Should load with correct initial state', () => {
-    cy.visit('login');
+    cy.getByTestId('email-status').should('have.attr', 'title', '入力必須です');
   });
 });
