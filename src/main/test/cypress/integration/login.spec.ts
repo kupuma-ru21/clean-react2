cfb740e9be04611894ec77b277c53ee0c33907d3
kupuma-ru21 <kupumaru21@gmail.com>
2021-07-04 14:21:33 +0900

    test: ensure login begin with correct initial state

--- src/main/test/cypress/integration/login.spec.ts
+++ src/main/test/cypress/integration/login.spec.ts
@@ -1,6 +1,13 @@
 describe('Login', () => {
   beforeEach(() => cy.visit('login'));
   it('Should load with correct initial state', () => {
-    cy.getByTestId('email-status').should('have.attr', 'title', '入力必須です');
+    cy.getByTestId('email-status')
+      .should('have.attr', 'title', '入力必須です')
+      .should('contain.text', '🔴');
+    cy.getByTestId('password-status')
+      .should('have.attr', 'title', '入力必須です')
+      .should('contain.text', '🔴');
+    cy.getByTestId('submit').should('have.attr', 'disabled');
+    cy.getByTestId('error-wrap').should('not.have.descendants');
   });
 });
