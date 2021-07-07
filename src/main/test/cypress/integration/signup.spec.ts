e3cc228ed1fffcd07386d95c4e7bd8a8e11240fb
kupuma-ru21 <kupumaru21@gmail.com>
2021-07-07 23:14:47 +0900

    test: ensure SignUp prevent submit if form is invalid

--- src/main/test/cypress/integration/signup.spec.ts
+++ src/main/test/cypress/integration/signup.spec.ts
@@ -117,4 +117,14 @@ describe('SignUp', () => {
     cy.getByTestId('submit').dblclick();
     FormHelper.testHttpCallsCount(1);
   });
+
+  it('Should not call if form is invalid', () => {
+    Http.mockOk();
+    cy.getByTestId('email')
+      .focus()
+      .type(faker.internet.email())
+      .type('{enter}');
+
+    FormHelper.testHttpCallsCount(0);
+  });
 });
