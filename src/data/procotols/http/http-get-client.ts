5db28159213875a71ec32e7daf249bbbc15a9753
kupuma-ru21 <kupumaru21@gmail.com>
2021-07-08 20:31:57 +0900

    feat: ensure RemoteLoadSurveyList calls HttpGetClient with correct URL

---
+++ src/data/procotols/http/http-get-client.ts
@@ -0,0 +1,5 @@
+export type HttpGetParams = { url: string };
+
+export interface HttpGetClient {
+  get: (params: HttpGetParams) => Promise<void>;
+}
--- src/data/procotols/http/index.ts
+++ src/data/procotols/http/index.ts
@@ -1,2 +1,3 @@
 export * from './http-post-client';
 export * from './http-reponse';
+export * from './http-get-client';
