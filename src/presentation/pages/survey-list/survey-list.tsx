bb82944dc8dee681468a45ba0365e51534000b42
kupuma-ru21 <kupumaru21@gmail.com>
2021-07-18 11:28:09 +0900

    feat: add skelton shimmer effect

--- src/presentation/pages/survey-list/survey-list.tsx
+++ src/presentation/pages/survey-list/survey-list.tsx
@@ -21,30 +21,7 @@ const SurveyList: React.FC = () => {
             </div>
             <footer>Ver Resulttado</footer>
           </li>
-          <li>
-            <div className={Styles.surveyContent}>
-              <Icon className={Styles.iconWrap} iconName={IconName.thumbDown} />
-              <time>
-                <span className={Styles.day}>22</span>
-                <span className={Styles.month}>10</span>
-                <span className={Styles.year}>2020</span>
-              </time>
-              <p>Qual e seu framework web favorite?</p>
-            </div>
-            <footer>Ver Resulttado</footer>
-          </li>
-          <li>
-            <div className={Styles.surveyContent}>
-              <Icon className={Styles.iconWrap} iconName={IconName.thumbDown} />
-              <time>
-                <span className={Styles.day}>22</span>
-                <span className={Styles.month}>10</span>
-                <span className={Styles.year}>2020</span>
-              </time>
-              <p>Qual e seu framework web favorite?</p>
-            </div>
-            <footer>Ver Resulttado</footer>
-          </li>
+          <li></li>
         </ul>
       </div>
       <Footer />
