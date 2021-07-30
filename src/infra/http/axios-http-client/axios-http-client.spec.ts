4fac1e34832719ec208915af42b6727312bf070e
kupuma-ru21 <kupumaru21@gmail.com>
2021-07-31 00:01:59 +0900

    test: ensure AxiosHttpClient calls axios with correct values

--- src/infra/http/axios-http-client/axios-http-client.spec.ts
+++ src/infra/http/axios-http-client/axios-http-client.spec.ts
@@ -46,7 +46,9 @@ describe('AxiosHttpClient', () => {
       const request = mockGetRequest();
       const { sut, mockedAxios } = makeSut();
       await sut.get(request);
-      expect(mockedAxios.get).toHaveBeenCalledWith(request.url);
+      expect(mockedAxios.get).toHaveBeenCalledWith(request.url, {
+        headers: request.headers,
+      });
     });
 
     test('Should return correct response on axios.get', async () => {
