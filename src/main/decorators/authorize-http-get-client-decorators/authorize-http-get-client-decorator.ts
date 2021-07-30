565271f34dd0215b94530cccff5667e573a3c59d
kupuma-ru21 <kupumaru21@gmail.com>
2021-07-31 00:42:04 +0900

    feat: ensure AuthorizeHttpGetClientDecorator not add token if GetStorage is invalid

--- src/main/decorators/authorize-http-get-client-decorators/authorize-http-get-client-decorator.ts
+++ src/main/decorators/authorize-http-get-client-decorators/authorize-http-get-client-decorator.ts
@@ -6,9 +6,14 @@ import {
 } from '@/data/procotols/http';
 
 export class AuthorizeHttpGetClientDecorator implements HttpGetClient {
-  constructor(private readonly getStorage: GetStorage) {}
+  constructor(
+    private readonly getStorage: GetStorage,
+    private readonly httpGetClient: HttpGetClient
+  ) {}
+
   async get(params: HttpGetParams): Promise<HttpResponse> {
     this.getStorage.get('account');
+    await this.httpGetClient.get(params);
     return null;
   }
 }
