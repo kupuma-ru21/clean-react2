40ca7256c59c3267b43b55ede1c0d402758ef6e3
kupuma-ru21 <kupumaru21@gmail.com>
2021-08-04 21:47:33 +0900

    refactor: change SurveyResult to return inCurrentAccountAnswer

--- src/data/test/mock-remote-survey-result.ts
+++ src/data/test/mock-remote-survey-result.ts
@@ -11,11 +11,13 @@ export const mockRemoteSurveyResultModel = (): RemoteLoadSurveyResult.Model => {
         answer: faker.random.word(),
         count: faker.datatype.number(),
         percent: faker.datatype.number(100),
+        inCurrentAccountAnswer: faker.datatype.boolean(),
       },
       {
         answer: faker.random.word(),
         count: faker.datatype.number(),
         percent: faker.datatype.number(100),
+        inCurrentAccountAnswer: faker.datatype.boolean(),
       },
     ],
   };
