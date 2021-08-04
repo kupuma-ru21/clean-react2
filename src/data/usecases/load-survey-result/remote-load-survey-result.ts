6c757dfea326daec575ca4b2bd38772a145f782d
kupuma-ru21 <kupumaru21@gmail.com>
2021-08-04 21:16:11 +0900

    feat: ensure RemoteLoadSurveyResult throws UnexpectedError on 404

--- src/data/usecases/load-survey-result/remote-load-survey-result.ts
+++ src/data/usecases/load-survey-result/remote-load-survey-result.ts
@@ -1,5 +1,5 @@
+import { AccessDeniedError, UnexpectedError } from '@/domain/errors';
 import { HttpGetClient, HttpStatusCode } from '@/data/procotols/http';
-import { AccessDeniedError } from '@/domain/errors';
 
 export class RemoteLoadSurveyResult {
   constructor(
@@ -12,8 +12,10 @@ export class RemoteLoadSurveyResult {
     switch (httpResponse.statusCode) {
       case HttpStatusCode.ok:
         break;
-      default:
+      case HttpStatusCode.forbidden:
         throw new AccessDeniedError();
+      default:
+        throw new UnexpectedError();
     }
   }
 }
