f30ee2a123ee19d24dce0432a08685b75b2e367b
kupuma-ru21 <kupumaru21@gmail.com>
2021-08-05 23:32:05 +0900

    test: ensure SurveyResult calls LoadSurveyResult

--- src/presentation/pages/survey-result/survey-result.spec.tsx
+++ src/presentation/pages/survey-result/survey-result.spec.tsx
@@ -1,10 +1,13 @@
 import React from 'react';
-import { render, screen } from '@testing-library/react';
+import { render, screen, waitFor } from '@testing-library/react';
 import { SurveyResult } from '@/presentation/pages';
 import { ApiContext } from '@/presentation/context';
-import { mockAccountModel } from '@/domain/test';
+import { LoadSurveyResultSpy, mockAccountModel } from '@/domain/test';
 
-const makeSut = (): void => {
+type SutTypes = { loadSurveyResultSpy: LoadSurveyResultSpy };
+
+const makeSut = (): SutTypes => {
+  const loadSurveyResultSpy = new LoadSurveyResultSpy();
   render(
     <ApiContext.Provider
       value={{
@@ -12,9 +15,11 @@ const makeSut = (): void => {
         getCurrentAccount: () => mockAccountModel(),
       }}
     >
-      <SurveyResult />
+      <SurveyResult loadSurveyResult={loadSurveyResultSpy} />
     </ApiContext.Provider>
   );
+
+  return { loadSurveyResultSpy };
 };
 
 describe('SurveyResult Cmponent', () => {
@@ -24,5 +29,12 @@ describe('SurveyResult Cmponent', () => {
     expect(surveyResult.childElementCount).toBe(0);
     expect(screen.queryByTestId('error')).not.toBeInTheDocument();
     expect(screen.queryByTestId('loading')).not.toBeInTheDocument();
+    await waitFor(() => surveyResult);
+  });
+
+  test('Should call LoadSurveyResult', async () => {
+    const { loadSurveyResultSpy } = makeSut();
+    await waitFor(() => screen.getByTestId('survey-result'));
+    expect(loadSurveyResultSpy.callsCount).toBe(1);
   });
 });
