9b7a3a12c9580755ea4fe96a8565887bf72e4218
kupuma-ru21 <kupumaru21@gmail.com>
2021-08-07 02:04:56 +0900

    test: ensure SurveyResult reloads on reload button click

--- src/main/test/cypress/integration/survey-result.spec.ts
+++ src/main/test/cypress/integration/survey-result.spec.ts
@@ -8,6 +8,10 @@ const mockUnexpectedError = (): void => {
   return Http.mockServerError(path, 'GET');
 };
 
+const mockSuccess = (): void => {
+  return Http.mockOk(path, 'GET', 'fx:survey-result');
+};
+
 describe('SurveyResult', () => {
   beforeEach(() => {
     cy.fixture('account').then((account) => {
@@ -26,5 +30,17 @@ describe('SurveyResult', () => {
       'contain.text',
       'Algo de errado acounteceu. Tente novaente em breve.'
     );
+  });
+
+  it('Should reload on button click', () => {
+    mockUnexpectedError();
+    cy.visit('/surveys/any_id');
+    cy.getByTestId('error').should(
+      'contain.text',
+      'Algo de errado acounteceu. Tente novaente em breve.'
+    );
+    mockSuccess();
+    cy.getByTestId('reload').click();
+    cy.getByTestId('question').should('exist');
   });
 });
