0e06012d9c013bbc81f90de76e1a847e9429aecc
kupuma-ru21 <kupumaru21@gmail.com>
2021-08-07 01:53:32 +0900

    test: ensure SurveyResult redirects to login if notoken is found

--- src/main/test/cypress/integration/private-routes.spec.ts
+++ src/main/test/cypress/integration/private-routes.spec.ts
@@ -8,4 +8,9 @@ describe('PrivateRoutes', () => {
     cy.visit('');
     Helper.testUrl('/login');
   });
+
+  it('Should logout if survey-result has no token', () => {
+    cy.visit('/surveys/any_id');
+    Helper.testUrl('/login');
+  });
 });
